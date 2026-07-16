508658a933f29168bf8e0404169e3a69858bc5e8
sebastian flores,edwin ortiz,giusepe sanchez <sebastianfloresventura7@gmail.com>
2026-07-15 19:48:51 -0500

    Proyecto

--- proyecto.py
+++ proyecto.py
@@ -1,4 +1,15 @@
-class edu:
-    def __init__(self,nombre,apellido):
-        self.nombre = nombre
-        self.apellido = apellido
+print("=====Bienvenido al Programa=====")
+print("(1)Registrarse")
+print("(2)Buscar es")
+print("(3)Editar información")
+print("(4)Eliminar registros")
+print("(5)Mostrar todos los estudiantes.")
+opcion=int(input(("Elige una opción")))
+
+if opcion == 1:
+    nom=input("ingrese sus nombres : ")
+    ape=input("ingrese sus apellidos: ")
+    dni=int(input("ingrese su numero de DNI: "))
+    nota=float(input("ingrese su nota: "))
+    
+
